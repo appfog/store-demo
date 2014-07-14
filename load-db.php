275cda03725a3a15b030b97798a009db9bd4f68d
Josh Hundley <josh.hundley@gmail.com>
2014-07-14 14:41:27 -0700

    now with database load script

---
+++ load-db.php
@@ -0,0 +1,42 @@
+<?php
+
+$services = getenv("VCAP_SERVICES");
+$services_json = json_decode($services,true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+
+$filename = 'drupal.sql';
+$mysql_host = $mysql_config['hostname'];
+$mysql_username = $mysql_config['user'];
+$mysql_password = $mysql_config['password'];
+$mysql_database = $mysql_config['name'];
+
+// Connect to MySQL server
+mysql_connect($mysql_host, $mysql_username, $mysql_password) or die('Error connecting to MySQL server: ' . mysql_error());
+// Select database
+mysql_select_db($mysql_database) or die('Error selecting MySQL database: ' . mysql_error());
+
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+if (substr($line, 0, 2) == '--' || $line == '')
+    continue;
+
+// Add this line to the current segment
+$templine .= $line;
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // Perform the query
+    mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
+ echo "Tables imported successfully";
+
+?>
